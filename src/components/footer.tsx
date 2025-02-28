fccbf7b50d74a95e0d56bb7dcc3198091b7575ef
David Ballester <david.ballester.mena@gmail.com>
2025-02-28 22:15:34 +0100

    Added LinkedIn link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,35 @@
-import { Box, Button } from "@chakra-ui/react";
-import { LuGithub } from "react-icons/lu";
+import { Button, HStack, Link, Text, VStack } from "@chakra-ui/react";
+import { LuCopyright, LuGithub, LuLinkedin } from "react-icons/lu";
 
 export function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
-    <Box as="footer">
-      <Button asChild variant="ghost">
-        <a href="https://github.com/davidballester">
-          <LuGithub /> Visit my GitHub profile
-        </a>
-      </Button>
-    </Box>
+    <VStack as="footer" gap="6">
+      <HStack gap="9">
+        <Button asChild variant="ghost" size="lg">
+          <a href="https://github.com/davidballester">
+            <LuGithub /> GitHub profile
+          </a>
+        </Button>
+        <Button asChild variant="ghost" size="lg">
+          <a href="https://www.linkedin.com/in/david-ballester-mena-274a2034/">
+            <LuLinkedin /> LinkedIn profile
+          </a>
+        </Button>
+      </HStack>
+      <Text textStyle="sm" color="gray.400">
+        Built with love and{" "}
+        <Link href="https://nextjs.org/" target="_blank">
+          Next.js
+        </Link>
+        .
+      </Text>
+      <HStack gap="1" color="gray.400">
+        <LuCopyright />
+        <Text textStyle="sm">
+          David Ballester Mena {currentYear}. All rights reserved.
+        </Text>
+      </HStack>
+    </VStack>
   );
 }
